5230080f2a039d2178cff8b68682d49d22ca8c46
Mikiyas <mikiyaszenebe10@gmail.com>
2022-09-10 07:34:08 -0700

    updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Input, Button } from "@mui/material";
-import { MdAddPhotoAlternate } from "react-icons/md";
+
 function Login() {
   return (
     <div className="form-container">
