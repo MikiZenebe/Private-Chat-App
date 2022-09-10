9688a68efa7ad034f635bce3de05039c8cca3840
Mikiyas <mikiyaszenebe10@gmail.com>
2022-09-10 08:27:55 -0700

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Button } from "@mui/material";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <span className="logo">Mik Chat</span>
+
+      <div className="user">
+        <img src="" alt="" />
+        <span>Micky</span>
+        <Button>Logout</Button>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import Navbar from "../components/Navbar";
+
 function Sidebar() {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <Navbar />
+    </div>
+  );
 }
 
 export default Sidebar;
