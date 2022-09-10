b95fc14f1438457ec93f819b78f1ef167b8d5e20
Mikiyas <mikiyaszenebe10@gmail.com>
2022-09-10 03:50:25 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 import "./style.scss";
 
 function App() {
   return (
     <div>
-      <Register />
+      {/* <Register /> */}
+      <Login />
     </div>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,25 @@
 import React from "react";
-
+import { Input, Button } from "@mui/material";
+import { MdAddPhotoAlternate } from "react-icons/md";
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="form-container">
+      <div className="form-wrapper">
+        {/* <img src={Logo} alt="logo" /> */}
+        <span className="logo">Mik Chat</span>
+        <span className="title">Login</span>
+
+        <form>
+          <Input type="email" placeholder="Email" />
+          <Input type="password" placeholder="Password" />
+
+          <Button variant="contained">Log In</Button>
+        </form>
+
+        <p>Don't have an account? Register</p>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
